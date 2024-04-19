313cf4eb6be61c3c267d28d3b55bfb8a5ce522e8
Sergey Eroshenko <sergey.eroshenko.nsk@gmail.com>
2024-04-19 18:23:16 +0300

    change dmc to dbc

--- src/run.py
+++ src/run.py
@@ -1,53 +1,50 @@
 # Import packages
-from dash import Dash, dash_table, dcc, callback, Output, Input
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+from glob import glob
 import pandas as pd
 import plotly.express as px
-import dash_mantine_components as dmc
+import dash_bootstrap_components as dbc
 
 # Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+files_list = sorted(glob("data/raw/*"))
 
-# Initialize the app - incorporate a Dash Mantine theme
-external_stylesheets = [dmc.theme.DEFAULT_COLORS]
+df = pd.read_excel(files_list[0], skiprows=3, sheet_name="Кандидаты")
+
+# Initialize the app - incorporate a Dash Bootstrap theme
+external_stylesheets = [dbc.themes.CERULEAN]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 # App layout
-app.layout = dmc.Container([
-    dmc.Title(
-        'My First App with Data, Graph, and Controls',
-        color="blue",
-        size="h3"
-    ),
-    dmc.RadioGroup(
-            [dmc.Radio(i, value=i) for i in ['pop', 'lifeExp', 'gdpPercap']],
-            id='my-dmc-radio-item',
-            value='lifeExp',
-            size="sm"
-        ),
-    dmc.Grid([
-        dmc.Col([
-            dash_table.DataTable(
-                data=df.to_dict('records'),
-                page_size=12,
-                style_table={'overflowX': 'auto'}
-            )
-        ], span=6),
-        dmc.Col([
-            dcc.Graph(figure={}, id='graph-placeholder')
-        ], span=6),
+app.layout = dbc.Container([
+    dbc.Row([
+        html.Div('Аналитика по процессам найма', className="text-primary text-center fs-3")
+    ]),
+
+    dbc.Row([
+        dcc.Dropdown(['NYC', 'MTL', 'SF'], 'NYC', id='demo-dropdown'),
+    ]),
+
+    dbc.Row([
+        dbc.Col([
+            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'})
+        ], width=6),
+
+        dbc.Col([
+            dcc.Graph(figure={}, id='my-first-graph-final')
+        ], width=6),
     ]),
 
 ], fluid=True)
 
 # Add controls to build the interaction
 @callback(
-    Output(component_id='graph-placeholder', component_property='figure'),
-    Input(component_id='my-dmc-radio-item', component_property='value')
+    Output(component_id='my-first-graph-final', component_property='figure'),
+    Input(component_id='radio-buttons-final', component_property='value')
 )
 def update_graph(col_chosen):
     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
     return fig
 
-# Run the App
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
